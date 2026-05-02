0b8911e00602bbcb53b2e1cdc08a0af03a117615
Kristof <neyt.kr@gmail.com>
2026-05-02 16:06:32 +0200

    test: increase test timeouts and update OS version source

--- tests/smoke_test/test_offline.py
+++ tests/smoke_test/test_offline.py
@@ -15,19 +15,23 @@ def _check_connectivity(shell, *, connected):
                 if connected:
                     return True
                 else:
-                    raise AssertionError(f"expecting disconnected but {target} is alive")
+                    raise AssertionError(
+                        f"expecting disconnected but {target} is alive"
+                    )
         except ExecutionError as exc:
             if not connected:
                 stdout = "\n".join(exc.stdout)
-                assert ("Network is unreachable" in stdout
-                        or "bad address" in stdout
-                        or "No response" in stdout)
+                assert (
+                    "Network is unreachable" in stdout
+                    or "bad address" in stdout
+                    or "No response" in stdout
+                )
 
     if connected:
         raise AssertionError(f"expecting connected but all targets are down")
 
 
-@pytest.mark.timeout(120)
+@pytest.mark.timeout(200)
 @pytest.mark.usefixtures("without_internet")
 def test_ha_runs_offline(shell):
     def check_container_running(container_name):
@@ -39,7 +43,7 @@ def test_ha_runs_offline(shell):
     # wait for supervisor to create network
     while True:
         if check_container_running("hassio_supervisor"):
-            nm_conns = shell.run_check('nmcli con show')
+            nm_conns = shell.run_check("nmcli con show")
             if "Supervisor" in " ".join(nm_conns):
                 break
         sleep(1)
@@ -47,14 +51,18 @@ def test_ha_runs_offline(shell):
     # To simulate situation where HAOS is not connected to internet, we need to add
     # default gateway to the supervisor connection. So we add a default route to
     # a non-existing IP address in the VM's subnet. Maybe there is a better way?
-    shell.run_check('nmcli con modify "Supervisor enp0s3" ipv4.addresses "192.168.76.10/24" '
-                    '&& nmcli con modify "Supervisor enp0s3" ipv4.gateway 192.168.76.1 '
-                    '&& nmcli device reapply enp0s3')
+    shell.run_check(
+        'nmcli con modify "Supervisor enp0s3" ipv4.addresses "192.168.76.10/24" '
+        '&& nmcli con modify "Supervisor enp0s3" ipv4.gateway 192.168.76.1 '
+        "&& nmcli device reapply enp0s3"
+    )
 
     _check_connectivity(shell, connected=False)
 
-    for _ in range(60):
-        if check_container_running("homeassistant") and check_container_running("hassio_cli"):
+    for _ in range(100):
+        if check_container_running("homeassistant") and check_container_running(
+            "hassio_cli"
+        ):
             break
         sleep(1)
     else:
--- tests/smoke_test/test_os_update.py
+++ tests/smoke_test/test_os_update.py
@@ -18,19 +18,28 @@ def test_init(shell, shell_json):
 
     # wait for important containers first
     while True:
-        if check_container_running("homeassistant") and check_container_running("hassio_supervisor"):
+        if check_container_running("homeassistant") and check_container_running(
+            "hassio_supervisor"
+        ):
             break
 
         sleep(1)
 
     # wait for the system ready and Supervisor at the latest version
     while True:
-        supervisor_info = "\n".join(shell.run_check("ha supervisor info --no-progress --raw-json || true"))
+        supervisor_info = "\n".join(
+            shell.run_check("ha supervisor info --no-progress --raw-json || true")
+        )
         # make sure not to fail when Supervisor is restarting
-        supervisor_info = json.loads(supervisor_info) if supervisor_info.startswith("{") else None
+        supervisor_info = (
+            json.loads(supervisor_info) if supervisor_info.startswith("{") else None
+        )
         # make sure not to fail when Supervisor is in setup state
         supervisor_data = supervisor_info.get("data") if supervisor_info else None
-        if supervisor_data and supervisor_data["version"] == supervisor_data["version_latest"]:
+        if (
+            supervisor_data
+            and supervisor_data["version"] == supervisor_data["version_latest"]
+        ):
             output = "\n".join(shell.run_check("ha os info || true"))
             if "System is not ready" not in output:
                 break
@@ -51,11 +60,18 @@ def test_os_update(shell, shell_json, target):
     shell.run_check("ha su reload --no-progress")
 
     # update OS to latest stable - in tests it should never be the same version
-    stable_version = shell_json("curl -sSL https://version.home-assistant.io/stable.json")["hassos"]["ova"]
+    stable_version = shell_json(
+        "curl -sSL https://raw.githubusercontent.com/PowerLabs-be/power-pilot-version/refs/heads/master/stable.json"
+    )["ppos"]["ova"]
 
     # Core (and maybe Supervisor) might be downloaded at this point, so we need to keep trying
     while True:
-        output = "\n".join(shell.run_check(f"ha os update --no-progress --version {stable_version} || true", timeout=120))
+        output = "\n".join(
+            shell.run_check(
+                f"ha os update --no-progress --version {stable_version} || true",
+                timeout=120,
+            )
+        )
         if "Don't have an URL for OTA updates" in output:
             shell.run_check("ha su reload --no-progress")
         elif "Command completed successfully" in output:
@@ -71,7 +87,9 @@ def test_os_update(shell, shell_json, target):
 
     # temporary needed for OS 17.0 -> 16.x path, where all containers must be re-downloaded
     while True:
-        if check_container_running("hassio_supervisor") and check_container_running("hassio_cli"):
+        if check_container_running("hassio_supervisor") and check_container_running(
+            "hassio_cli"
+        ):
             break
 
         sleep(1)
@@ -86,7 +104,9 @@ def test_os_update(shell, shell_json, target):
 
     # check the updated version
     os_info = shell_json("ha os info --no-progress --raw-json")
-    assert os_info["data"]["version"] == stable_version, "OS did not update successfully"
+    assert os_info["data"]["version"] == stable_version, (
+        "OS did not update successfully"
+    )
 
 
 @pytest.mark.dependency(depends=["test_os_update"])
